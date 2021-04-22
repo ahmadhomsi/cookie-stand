455dc904c226c9f59e3370cf7ad925a6acbef98f
ahmadhomsi <homsiahmad111@gmail.com>
2021-04-22 23:54:27 +0300

    new some ghang

--- app.js
+++ app.js
@@ -95,3 +95,17 @@ function generateRandomNumber(min, max) {
     random = Math.floor(random);
     return random;
 }
+var breakfast = ['banana',
+                'chicken wing',
+                'coffee',
+                'eye of newt',
+                'pancakes',
+                'avocado',
+                'fish biscuit',
+                'pecan',
+                'corn dog'];
+var indexOne = 3;
+var indexTwo = 4;
+var indexThree = 6;
+for (var i = indexOne; i < indexThree; i++) {
+  console.log(breakfast[i]);
